4d14daf68000e3e60d1013e6ff4963c7fcffcea0
discphy <dinophy@nate.com>
2024-04-23 08:04:09 +0900

    feat(add) : main py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import get_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
+
+db = {}
 
 
 @app.route("/")
@@ -12,8 +15,28 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    jobs = get_jobs(keyword)
+    if keyword is None:
+        return redirect("/")
+
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        jobs = get_jobs(keyword)
+        db[keyword] = jobs
+
     return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword is None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}_jobs.csv", as_attachment=True)
 
 
 app.run("0.0.0.0")
